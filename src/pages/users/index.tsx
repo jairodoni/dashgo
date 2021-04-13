a605f02891dc01aaf5a1495cac14df8e2da34dd7
Jairo Doni <jairo.doni97@gmail.com>
2021-04-13 16:25:05 -0300

    ajustes no fluxo de informações

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,9 +15,7 @@ import { GetServerSideProps } from "next";
 
 export default function UserList({ users }) {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers(page, {
-    initialData: users,
-  });
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -114,15 +112,4 @@ export default function UserList({ users }) {
       </Flex>
     </Box>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const { users, totalCount } = await getUsers(1);
-
-
-  return {
-    props: {
-      users,
-    }
-  }
 }
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -42,11 +42,10 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   };
 }
 
-export function useUsers(page: number, options: UseQueryOptions) {
+export function useUsers(page: number) {
   //Com arrow function executara o getUsers somente quando a Query for lançada
   //O useQueryguarda um valor em cache utilizando uma chave, ex:'users'
   return useQuery(['users', page], () => getUsers(page), {
     staleTime: 1000 * 60 * 10, //10 minuntos
-    ...options,
   });
 }
